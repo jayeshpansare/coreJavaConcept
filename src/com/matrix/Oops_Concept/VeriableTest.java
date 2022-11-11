cc54b409452c7a4a896ee10aaf34a10c61d885f2
jayesh <jayesh.pansare.l@gmail.com>
2022-11-11 21:32:19 +0530

    veriable

---
+++ src/com/matrix/Oops_Concept/VeriableTest.java
@@ -0,0 +1,19 @@
+package com.matrix.Oops_Concept;
+
+public class VeriableTest {
+    static final int MAX_NUMBER = 10;  // static Variables
+    String str = "jayesh";  // global Instance  Variables
+    boolean br = true;
+
+    public static void main(String[] args){
+        VeriableTest obj = new VeriableTest();
+        obj.addTwoNumbers();
+
+    }
+
+    public void addTwoNumbers(){
+        int minNumber = 20;  // Local Variables
+        System.out.println(br);
+        System.out.println(str);
+    }
+}
